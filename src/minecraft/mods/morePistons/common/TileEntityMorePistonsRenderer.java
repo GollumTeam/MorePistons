49f9e1b4d7bacb5aadce637b954a7e2155d43a9b
Damien Duboeuf <d.duboeuf@glowbl.com>
2013-09-17 10:15:47 +0200

    Useless import

--- src/minecraft/mods/morePistons/common/TileEntityMorePistonsRenderer.java
+++ src/minecraft/mods/morePistons/common/TileEntityMorePistonsRenderer.java
@@ -1,19 +1,19 @@
 package mods.morePistons.common;
 
 import net.minecraft.block.Block; // amq;
-import net.minecraft.tileentity.TileEntity; // any;
-import net.minecraft.tileentity.TileEntityPiston;
-import net.minecraft.util.Facing;
-import net.minecraft.util.MathHelper;
 import net.minecraft.block.BlockPistonBase; // aoa;
-import net.minecraft.block.BlockPistonExtension; // aob;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderBlocks; // bbb;
 import net.minecraft.client.renderer.RenderHelper; // arw;
 import net.minecraft.client.renderer.Tessellator; // baz;
-import net.minecraft.client.renderer.RenderBlocks; // bbb;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer; // bdx
-import net.minecraft.client.Minecraft;
-import org.lwjgl.opengl.GL11;
+import net.minecraft.tileentity.TileEntity; // any;
+import net.minecraft.tileentity.TileEntityPiston;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World; // yc;
+
+import org.lwjgl.opengl.GL11;
+// aob;
 
 public class TileEntityMorePistonsRenderer extends TileEntitySpecialRenderer {
 	
