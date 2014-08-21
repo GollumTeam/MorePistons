d4f507b9711003bfcfc133c861cfe588e172b020
SmeagolWorms4 <smeagolworms4@gmail.com>
2014-08-21 02:35:45 +0200

    import

--- src/minecraft/mods/morepistons/ModMorePistons.java
+++ src/minecraft/mods/morepistons/ModMorePistons.java
@@ -13,6 +13,8 @@ import mods.morepistons.common.block.BlockMorePistonsRod;
 import mods.morepistons.common.block.BlockMorePistonsSuper;
 import mods.morepistons.common.config.ConfigMorePistons;
 import mods.morepistons.common.tileentities.TileEntityMorePistons;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
