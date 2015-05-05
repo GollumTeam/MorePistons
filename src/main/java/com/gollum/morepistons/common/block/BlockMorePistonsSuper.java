5aea2d3fa50b38fd123615599774fa38b5bf92f3
SmeagolWorms4 <smeagolworms4@gmail.com>
2015-05-05 04:59:59 +0200

    Next block on super piston (WIP)

--- src/main/java/com/gollum/morepistons/common/block/BlockMorePistonsSuper.java
+++ src/main/java/com/gollum/morepistons/common/block/BlockMorePistonsSuper.java
@@ -44,6 +44,7 @@ public class BlockMorePistonsSuper extends BlockMorePistonsBase {
 
 		ArrayList<EMoveInfosExtend> infosExtend = super.listBlockExtend(world, x, y, z, orientation, currentOpened, lenghtOpened);
 		ArrayList<EMoveInfosExtend> upBlocks    = this.listUpBlocks(infosExtend, world, x, y, z, orientation, currentOpened, lenghtOpened, true);
+		ArrayList<EMoveInfosExtend> nextBlocks  = this.listNextBlocks(infosExtend, world, x, y, z, orientation, currentOpened, lenghtOpened, true);
 		
 		infosExtend.addAll(upBlocks);
 		
@@ -51,6 +52,7 @@ public class BlockMorePistonsSuper extends BlockMorePistonsBase {
 
 		all.addAll(infosExtend);
 		all.addAll(upBlocks);
+		all.addAll(nextBlocks);
 		
 		return all;
 	}
@@ -196,67 +198,35 @@ public class BlockMorePistonsSuper extends BlockMorePistonsBase {
 		return blocksTop;
 	}
 	
-//	protected ArrayList<EMoveInfosExtend> getListOrigin (World world, int x, int y, int z, int orientation, int lenghtOpened) {
-//		
-//		int xExtension = x;
-//		int yExtension = y;
-//		int zExtension = z;
-//		
-//		ArrayList<EMoveInfosExtend> blocksOrigin = new ArrayList<EMoveInfosExtend>();
-//		int size = lenghtOpened;
-//		
-//		for (int i = 0; i < (lenghtOpened + this.getMaxBlockMove ()) && size > 0; i++) {
-//			
-//			xExtension += Facing.offsetsXForSide[orientation];
-//			yExtension += Facing.offsetsYForSide[orientation];
-//			zExtension += Facing.offsetsZForSide[orientation];
-//			
-//			Block block = world.getBlock(xExtension, yExtension, zExtension);
-//			int metadata = world.getBlockMetadata(xExtension, yExtension, zExtension);
-//			
-//			if (this.isEmptyBlock(block)) {
-//				size--;
-//				continue;
-//			} else if (!this.isMovableBlock(block, world, xExtension, yExtension, zExtension)) {
-//				break;
-//			}
-//			blocksOrigin.add (new EMoveInfosExtend(xExtension, yExtension, zExtension, size));
-//		}
-//		Collections.reverse (blocksOrigin);
-//		
-//		return blocksOrigin;
-//	}
-//	
-//	
-//	protected ArrayList<EMoveInfosExtend> getListNextBlocks (ArrayList<EMoveInfosExtend> blocksOrigin, World world, int x, int y, int z, int orientation, int lenghtOpened) {
-//		
-//		int xExtension = x;
-//		int yExtension = y;
-//		int zExtension = z;
-//		
-//		ArrayList<EMoveInfosExtend> blocksList = new ArrayList<EMoveInfosExtend>();
-//		
-//		for (EMoveInfosExtend blockOrigin : blocksOrigin) {
-//			
-//			int xBlock;
-//			int yBlock;
-//			int zBlock;
-//			
-//			// On aprcour les 4 coins
-//			for (int o = 2; o <= 5; o++) {
-//				
-//				if (o == orientation || o == Facing.oppositeSide[orientation]) {
-//					continue;
-//				}
-//				
-//				
-//				xBlock = blockOrigin.x + Facing.offsetsXForSide[o];
-//				yBlock = blockOrigin.y;
-//				zBlock = blockOrigin.z + Facing.offsetsZForSide[o];
-//				
-//				Block block       = world.getBlock(xBlock, yBlock, zBlock);
-//				int metadata = world.getBlockMetadata(xBlock, yBlock, zBlock);
-//				
+	protected ArrayList<EMoveInfosExtend> listNextBlocks (ArrayList<EMoveInfosExtend> blocksOrigin, World world, int x, int y, int z, int orientation, int lenghtOpened, boolean extend) {
+		
+		int xExtension;
+		int yExtension;
+		int zExtension;
+		
+		ArrayList<EMoveInfosExtend> blocksList = new ArrayList<EMoveInfosExtend>();
+		
+		for (EMoveInfosExtend blockOrigin : blocksOrigin) {
+			
+			int xBlock;
+			int yBlock;
+			int zBlock;
+			
+			// On aprcour les 4 coins
+			for (int o = 2; o <= 5; o++) {
+				
+				if (o == orientation || o == Facing.oppositeSide[orientation]) {
+					continue;
+				}
+				
+				
+				xBlock = blockOrigin.position.x + Facing.offsetsXForSide[o];
+				yBlock = blockOrigin.position.y;
+				zBlock = blockOrigin.position.z + Facing.offsetsZForSide[o];
+				
+				Block block       = world.getBlock(xBlock, yBlock, zBlock);
+				int metadata = world.getBlockMetadata(xBlock, yBlock, zBlock);
+				
 //				if (
 //					block != null &&
 //					(
@@ -266,16 +236,16 @@ public class BlockMorePistonsSuper extends BlockMorePistonsBase {
 //				) {
 //					continue;
 //				}
-//				
-//				// nous avons la un block qui saccroche devrais dropper si c'etait un piston normal
-//				if (
-//					block != null && 
-//					block != Blocks.air &&
-//					(	this.isEmptyBlock(block) ||
-//						block instanceof BlockTrapDoor
-//					)
-//					&& this.isAttachOnNext (block, metadata, o)
-//				) {
+				
+				// nous avons la un block qui saccroche devrais dropper si c'etait un piston normal
+				if (
+					block != null && 
+					block != Blocks.air &&
+					(	this.isEmptyBlock(block) ||
+						block instanceof BlockTrapDoor
+					)
+					&& SuperPistonManager.instance.isAttachOnNext (block, metadata, o)
+				) {
 //					int moveBlock = 0;
 //					xExtension = xBlock;
 //					yExtension = yBlock;
@@ -304,133 +274,11 @@ public class BlockMorePistonsSuper extends BlockMorePistonsBase {
 //						blocksList.add(new EMoveInfosExtend(block, metadata, xExtension, yExtension, zExtension, moveBlock));
 //					}
 //				}
-//			}
-//			
-//		}
-//		
-//		return blocksList;
-//	}
-//	
-//	
-//	/**
-//	 * Retracte le block qui ets collé au piston si le piston est un sticky
-//	 * piston
-//	 * 
-//	 * @param world
-//	 * @param x
-//	 * @param y
-//	 * @param z
-//	 * @param length
-//	 */
-//	protected void retracSticky(World world, int x, int y, int z, int orientation, int length) {
-//		if (this.isSticky) {
-//
-//			int x2 = x + Facing.offsetsXForSide[orientation] * (length + 1);
-//			int y2 = y + Facing.offsetsYForSide[orientation] * (length + 1);
-//			int z2 = z + Facing.offsetsZForSide[orientation] * (length + 1);
-//			
-//			ArrayList<EMoveInfosExtend> blocksOrigin = new ArrayList<BlockMorePistonsBase.EMoveInfosExtend>();
-//			blocksOrigin.add(new EMoveInfosExtend(x2, y2, z2, length));
-//			
-//			ArrayList<EMoveInfosExtend> blocksTop    = this.getListUpBlocks(blocksOrigin, world, x, y, z, Facing.oppositeSide[orientation], length);
-//			ArrayList<EMoveInfosExtend> blocksNext   = this.getListNextBlocks(blocksOrigin, world, x, y, z, Facing.oppositeSide[orientation], length);
-//			
-//			super.retracSticky(world, x, y, z, orientation, length);
-//			
-//			// Dépalcement des élement Top
-//			for (EMoveInfosExtend blockTop : blocksTop) {
-//				world.setBlock(blockTop.x, blockTop.y, blockTop.z, Blocks.piston_extension, blockTop.metadata, 2);
-//				TileEntity teBlock = new TileEntityMorePistons (blockTop.block, blockTop.metadata, Facing.oppositeSide[orientation], true, false, blockTop.move, false);
-//				world.setTileEntity(blockTop.x, blockTop.y, blockTop.z, teBlock);
-//			}
-//			// Dépalcement des élement a coté
-//			for (EMoveInfosExtend blockNext : blocksNext) {
-//				world.setBlock(blockNext.x, blockNext.y, blockNext.z, Blocks.piston_extension, blockNext.metadata, 2);
-//				TileEntity teBlock = new TileEntityMorePistons (blockNext.block, blockNext.metadata, Facing.oppositeSide[orientation], true, false, blockNext.move, false);
-//				world.setTileEntity(blockNext.x, blockNext.y, blockNext.z, teBlock);
-//			}
-//			
-//		}
-//	}
-//	
-//	/**
-//	 * Ouvre un piston de la taille voulu
-//	 * @param world
-//	 * @param x
-//	 * @param y
-//	 * @param z
-//	 * @param orientation
-//	 * @param lenghtOpened
-//	 */
-//	protected void extend(World world, int x, int y, int z, int orientation, int lenghtOpened) {
-//		
-//		
-//		ArrayList<EMoveInfosExtend> blocksOrigin = this.getListOrigin(world, x, y, z, orientation, lenghtOpened);
-//		ArrayList<EMoveInfosExtend> blocksTop    = this.getListUpBlocks(blocksOrigin, world, x, y, z, orientation, lenghtOpened);
-//		ArrayList<EMoveInfosExtend> blocksNext   = this.getListNextBlocks(blocksOrigin, world, x, y, z, orientation, lenghtOpened);
-//		
-//		super.extend(world, x, y, z, orientation, lenghtOpened);
-//		
-//		// Dépalcement des élement Top
-//		for (EMoveInfosExtend blockTop : blocksTop) {
-//			world.setBlock(blockTop.x, blockTop.y, blockTop.z, Blocks.piston_extension, blockTop.metadata, 2);
-//			TileEntity teBlock = new TileEntityMorePistons (blockTop.block, blockTop.metadata, orientation, true, false, blockTop.move, false);
-//			world.setTileEntity(blockTop.x, blockTop.y, blockTop.z, teBlock);
-//		}
-//		// Dépalcement des élement a coté
-//		for (EMoveInfosExtend blockNext : blocksNext) {
-//			world.setBlock(blockNext.x, blockNext.y, blockNext.z, Blocks.piston_extension, blockNext.metadata, 2);
-//			TileEntity teBlock = new TileEntityMorePistons (blockNext.block, blockNext.metadata, orientation, true, false, blockNext.move, false);
-//			world.setTileEntity(blockNext.x, blockNext.y, blockNext.z, teBlock);
-//		}
-//		
-//		
-//	}
-//
-//	protected int convertOrientationFromTorch (int o) {
-//		switch (o) {
-//			case 1: return 5;
-//			case 2: return 4;
-//			case 3: return 3;
-//			case 4: return 2;
-//			default: return 0;
-//		}
-//	}
-//	
-//	protected int convertOrientationFromTripe (int o) {
-//		switch (o) {
-//			case 0: return 3;
-//			case 1: return 4;
-//			case 2: return 2;
-//			case 3: return 5;
-//			default: return 0;
-//		}
-//	}
-//	
-//	/**
-//	 * @param id
-//	 * @param metadata
-//	 * @return
-//	 */
-//	protected boolean isAttachOnNext (Block block, int metadata, int orientation) {
-//		
-//		if (block == null || block == Blocks.air) {
-//			return false;
-//		}
-////		
-//		if (
-//			(block instanceof BlockLadder       && metadata == orientation) ||                                    // Les echelles
-//			(block instanceof BlockTripWireHook && this.convertOrientationFromTripe (metadata)       == orientation) || // Les piège
-//			(block instanceof BlockButton       && this.convertOrientationFromTorch (metadata)       == orientation) || // Les boutons
-//			(block instanceof BlockTorch        && this.convertOrientationFromTorch (metadata)       == orientation) || // Les Torches charbons et Redstones
-//			(block instanceof BlockLever        && this.convertOrientationFromTorch (metadata)       == orientation) || // Les leviers
-//			(block instanceof BlockTrapDoor     && ((metadata & 0x3) + 2 == orientation)) || // Les leviers
-//			false
-//			
-//		) {
-//			return true;
-//		}
-//		
-//		return false;
-//	}
+			}
+			
+		}
+		
+		return blocksList;
+	}
+	
 }
--- src/main/java/com/gollum/morepistons/common/block/superpiston/SuperPistonManager.java
+++ src/main/java/com/gollum/morepistons/common/block/superpiston/SuperPistonManager.java
@@ -47,4 +47,47 @@ public class SuperPistonManager {
 		
 		return false;
 	}
+	
+	protected int convertOrientationFromTorch (int o) {
+		switch (o) {
+			case 1: return 5;
+			case 2: return 4;
+			case 3: return 3;
+			case 4: return 2;
+			default: return 0;
+		}
+	}
+	
+	protected int convertOrientationFromTripe (int o) {
+		switch (o) {
+			case 0: return 3;
+			case 1: return 4;
+			case 2: return 2;
+			case 3: return 5;
+			default: return 0;
+		}
+	}
+	
+	public boolean isAttachOnNext (Block block, int metadata, int orientation) {
+		
+		if (block == null || block == Blocks.air) {
+			return false;
+		}
+		
+		if (
+			(block instanceof BlockLadder       && metadata == orientation) ||                                    // Les echelles
+			(block instanceof BlockTripWireHook && this.convertOrientationFromTripe (metadata)       == orientation) || // Les piège
+			(block instanceof BlockButton       && this.convertOrientationFromTorch (metadata)       == orientation) || // Les boutons
+			(block instanceof BlockTorch        && this.convertOrientationFromTorch (metadata)       == orientation) || // Les Torches charbons et Redstones
+			(block instanceof BlockLever        && this.convertOrientationFromTorch (metadata)       == orientation) || // Les leviers
+			(block instanceof BlockTrapDoor     && ((metadata & 0x3) + 2 == orientation)) || // Les leviers
+			false
+			
+		) {
+			return true;
+		}
+		
+		return false;
+	}
+	
 }
