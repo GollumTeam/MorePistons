438e4b7065524fa4174a327776f6a0904929665f
SmeagolWorms4 <smeagolworms4@gmail.com>
2016-02-08 21:38:59 +0100

    Update GCL

--- src/main/java/com/gollum/morepistons/inits/ModBlocks.java
+++ src/main/java/com/gollum/morepistons/inits/ModBlocks.java
@@ -15,7 +15,6 @@ import com.gollum.morepistons.common.block.BlockMorePistonsSuper;
 import com.gollum.morepistons.common.block.BlockMorePistonsVanilla;
 import com.gollum.morepistons.common.block.BlockMorePistonsVanillaProxy;
 import com.gollum.morepistons.common.building.handler.BlockMorePistonsBuildingHandler;
-import com.gollum.morepistons.common.building.handler.BlockMorePistonsVanillaProxyBuildingHandler;
 
 public class ModBlocks {
 
@@ -223,9 +222,8 @@ public class ModBlocks {
 	}
 	
 	private static void initHandlerRotation () {
-
+		
 		BuildingBlockRegistry.register(new BlockMorePistonsBuildingHandler());
-		BuildingBlockRegistry.register(new BlockMorePistonsVanillaProxyBuildingHandler());
 		
 	}
 }
